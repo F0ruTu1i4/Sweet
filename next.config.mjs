ce25ed361cc776b0d7fa48d55c646628d590fb5f
F0ruTu1i4 <nowshinanjumtajkiya@proton.me>
2025-11-17 02:55:40 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Add this line:
+  output: 'export',
+
+  // You might have other settings here, like reactStrictMode:
+  reactStrictMode: true,
 };
 
 export default nextConfig;
